ba330a0024f49eb8cb754b43b35cf25bd0e1367e
ansjcy <168762116@qq.com>
2016-08-12 14:53:55 -0700

    start analysis

--- summer2016_PY/parse.py
+++ summer2016_PY/parse.py
@@ -1,7 +1,7 @@
 import csv
 
-readerEdge = csv.reader(file("/Users/anakin/Downloads/data/karate.edges.csv", "rb"))
-readerNode = csv.reader(file("/Users/anakin/Downloads/data/karate.nodes.csv", "rb"))
+readerEdge = csv.reader(file("/Users/anakin/Downloads/data_test/adjnoun.edges.csv", "rb"))
+readerNode = csv.reader(file("/Users/anakin/Downloads/data_test/adjnoun.nodes.csv", "rb"))
 
 
 
@@ -17,7 +17,7 @@ for line in readerEdge:
         continue
     edges.append([line[0], line[1]])
 
-outputFile = open("/Users/anakin/Desktop/haha.json", "w")
+outputFile = open("/Users/anakin/Desktop/adjnoun.json", "w")
 outputFile.write("{\n\"nodes\":[\n")
 for i in range(0, int(maxNode)):
     outputFile.write("{\"id\":\""+str(i)+"\", \"group\": 1},\n")
